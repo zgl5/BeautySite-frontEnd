55a08cbd6047335f0d710ac6868fa937ca804077
zgl5 <ozgul.karaman@gmail.com>
2023-06-01 12:24:03 -0400

    aboutpage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,22 @@
+import React from 'react';
+
 export default function About(props) {
-    return (
-      <>
-        <h1>Welcome to about</h1>
-              
-      </>
-    );
-  }
+  const slidingTextStyle = {
+    overflow: 'hidden',
+    animation: 'slide 10s linear infinite'
+  };
+
+  return (
+    <>
+      <h1>Welcome to About</h1>
+      <div style={slidingTextStyle}>
+        <p>
+        <h1>Under Construction</h1>
+<p>This website is currently under construction. 
+  We apologize for any inconvenience caused.
+   Please check back soon for updates.</p>
+        </p>
+      </div>
+    </>
+  );
+}
